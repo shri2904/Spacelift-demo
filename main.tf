57a576e3cc8cbc789f02a97e5f1c7eb8ef6fe370
shri2904 <shrihariharidas73@gmail.com>
2024-06-07 06:12:38 +0000

    added vpc bloc

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_vpc" "my_vpc" {
+  cidr_bloc = "10.0.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
